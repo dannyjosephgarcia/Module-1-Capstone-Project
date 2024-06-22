49e18dfc2e5c99c4be84baee0ae1cee80481fe15
musicmaster81 <106444995+musicmaster81@users.noreply.github.com>
2024-06-22 10:51:59 -0500

    LOCAL-1: Missing Anthem Words

--- tests/test_my_team.py
+++ tests/test_my_team.py
@@ -5,11 +5,12 @@ COUNTRY = 'USA'
 SPORT_NAME = 'Hockey'
 TEAM_NAME = 'Blackhawks'
 BEST_PLAYER = 'Connor Bedard'
+ANTHEM_WORDS = 'O say can you see'
 
 
 class TestMyTeam(unittest.TestCase):
     def setUp(self):
-        self.my_team = MyTeam(COUNTRY, SPORT_NAME, TEAM_NAME, BEST_PLAYER)
+        self.my_team = MyTeam(COUNTRY, SPORT_NAME, TEAM_NAME, BEST_PLAYER, ANTHEM_WORDS)
 
     def test_my_team_get_best_player(self):
         player_string = self.my_team.get_best_player()
